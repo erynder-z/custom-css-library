8633fa51b168e08310f94a7ed0b0924904053947
eryner-z <stfn.bgr@gmail.com>
2022-02-15 20:56:34 +0100

    purge unused css rules

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,17 @@
 const { src, dest, watch, series } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const purgecss = require('gulp-purgecss');
 
 //compile scss to css-folder
 function buildStyles() {
-  return src('eryngi/**/*.scss').pipe(sass()).pipe(dest('css'));
+  return src('eryngi/**/*.scss')
+    .pipe(sass())
+    .pipe(purgecss({ content: ['*.html'] })) // purge unused css rules from compiled css-file
+    .pipe(dest('css'));
 }
 
 function watchTask() {
-  watch(['eryngi/**/*.scss'], buildStyles);
+  watch(['eryngi/**/*.scss', '*.html'], buildStyles);
 }
 
 exports.default = series(buildStyles, watchTask);
