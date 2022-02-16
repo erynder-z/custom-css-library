a30597339bfff9415dccfefd9a09972b17b51400
eryner-z <stfn.bgr@gmail.com>
2022-02-16 17:25:50 +0100

    allow custom overrides

--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,18 @@ const purgecss = require('gulp-purgecss');
 
 //compile scss to css-folder
 function buildStyles() {
-  return src('eryngi/**/*.scss')
-    .pipe(sass())
-    .pipe(purgecss({ content: ['*.html'] })) // purge unused css rules from compiled css-file
-    .pipe(dest('css'));
+  return (
+    src('sass/**/*.scss')
+      //return src('eryngi/**/*.scss')
+      .pipe(sass())
+      .pipe(purgecss({ content: ['*.html'] })) // purge unused css rules from compiled css-file
+      .pipe(dest('css'))
+  );
 }
 
 function watchTask() {
-  watch(['eryngi/**/*.scss', '*.html'], buildStyles);
+  watch(['sass/**/*.scss', '*.html'], buildStyles);
+  //watch(['eryngi/**/*.scss', '*.html'], buildStyles);
 }
 
 exports.default = series(buildStyles, watchTask);
